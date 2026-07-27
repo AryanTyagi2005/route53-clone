0dd421a6f2c3c70659ad2787a1f7076ff67b4611
Aryan Tyagi <aryantyagi.cs@gmail.com>
2026-07-27 16:16:13 +0530

    Backend Deploy Fix

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://route53-clone-api-ywhf.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
